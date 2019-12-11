18ade03f2f386bb872b50bbede57c68cd17036ef
xiaowuge007 <lihongqi_elign@163.com>
2019-12-11 10:05:06 +0800

    新增js方法

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 [2.观察者模式、发布订阅模式](https://github.com/xiaowuge007/web-accumulate/issues/2)  
 [3.用js自定义一个事件，可以监听和移除](https://github.com/xiaowuge007/web-accumulate/issues/3)  
 [4. js实现链式调用](https://github.com/xiaowuge007/web-accumulate/issues/4)  
+[5.js深拷贝](https://github.com/xiaowuge007/web-accumulate/issues/5)  
+[6.扁平化去重](https://github.com/xiaowuge007/web-accumulate/issues/6)  
+[7.深度优先遍历、广度优先遍历](https://github.com/xiaowuge007/web-accumulate/issues/7)  
+[8.js面向对象继承](https://github.com/xiaowuge007/web-accumulate/issues/7)  
+[9.冒泡排序及优化](https://github.com/xiaowuge007/web-accumulate/issues/9)  
--- js/little.js
+++ js/little.js
@@ -228,3 +228,339 @@ setTimeout(()=>{
     person.eat('晚饭')
 })
 //eat:午饭 drink:可乐 sleep:5s go:商场 eat:晚饭
+
+
+/*
+* 深度优先遍历、广度优先遍历
+* */
+let treeArr = [
+    {
+        name: '1',
+        children: [
+            {
+                name: '1-1',
+                children: [{name:'1-1-1'}]
+            },{
+                name: '1-2',
+                children: [{name:'1-2-1'},{name:'1-2-2'},{name:'1-2-3'}]
+            }
+        ]
+    },{
+        name: '2',
+        children: [
+            {
+                name: '2-1',
+                children: [{name:'2-1-1'},{name:'2-1-2'},{name:'2-1-3'}]
+            },{
+                name: '2-2',
+                children: [{name:'2-2-1'},{name:'2-2-2'},{name:'2-2-3',children:[{name:'2-2-3-1'}]}]
+            }
+        ]
+    },{
+        name: '3',
+        children: [
+            {
+                name: '3-1',
+                children: [{name:'3-1-1'},{name:'3-1-2'},{name:'3-1-3'}]
+            }
+        ]
+    }
+]
+//深度优先遍历(递归)
+function each1(arr,newArr = []) {
+    for(let i = 0;i< arr.length;i++){
+        newArr.push({name:arr[i].name})
+        if(arr[i].children){
+            each1(arr[i].children,newArr)
+        }
+    }
+    return newArr;
+}
+console.log(each1(treeArr))
+//深度优先遍历(非递归)
+function each2(arr) {
+    arr = arr.map(val=>val)
+    let newArr = [];
+    while(arr.length){
+        let item = arr.shift()
+        newArr.push({name:item.name})
+        if(item.children){
+            for(let i = item.children.length-1;i>=0;i--){
+                arr.unshift(item.children[i])
+            }
+        }
+    }
+    return newArr;
+}
+console.log(each2(treeArr))
+
+//广度优先遍历
+function each3(arr) {
+    arr = arr.map(val=>val)
+    let newArr = [];
+    while(arr.length){
+        let item = arr.shift()
+        newArr.push({name:item.name})
+        if(item.children){
+            for(let i = 0;i<item.children.length;i++){
+                arr.push(item.children[i])
+            }
+        }
+    }
+    return newArr;
+}
+console.log(each3(treeArr))
+
+/*
+* 冒泡排序及优化
+* */
+//普通冒泡排序
+let arr2 = [3,5,1,4,2,6,9,7]
+function sort1(arr) {
+    for(let i = 0;i<arr.length;i++){
+        for(let j =i+1;j<arr.length;j++){
+            if(arr[i]>arr[j]){
+                let val = arr[i];
+                arr[i] = arr[j]
+                arr[j] = val
+            }
+        }
+    }
+    return arr;
+}
+console.log(sort1(arr2))
+//优化方案
+//每次最大值放到最右后，会将本轮最后一个操作的位置作为下一轮的终点，可以减少不必要的一些冒泡
+function sort2(arr) {
+    let i = arr.length - 1;
+    while (i > 0) {
+        let pos = 0;
+        for (let j = 0; j < i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                pos = j;
+                const temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+        i = pos;
+    }
+    console.log(arr);
+}
+sort2(arr2);
+
+/*
+* 面向对象的继承
+* */
+//1.原型继承
+!function () {
+    function Parent() {
+
+    }
+    Parent.prototype.name = 'tom'
+    Parent.prototype.eat = function () {}
+    function Children() {
+    }
+    Children.prototype = new Parent()
+    Children.prototype.constructor = Children;
+}();
+/*
+* 缺点：一个子类更改了父类原型上的引用类型，导致所有子类都更改
+* 缺点：子类无法传参给父类
+* */
+
+//2.构造函数继承
+!function () {
+    function Parent(name) {
+        this.name = name;
+        this.eat = function () {}
+    }
+    function Children(name) {
+        Parent.call(this,name)
+    }
+}();
+/*
+*补足方法1中的缺点
+*缺点：不能继承原型属性
+* */
+//3.组合继承
+(function () {
+    function Parent(name) {
+        this.name = name;
+    }
+    Parent.prototype.eat = function () {}
+    Parent.prototype.arr = [1]
+    function Children(name) {
+        Parent.call(this,name)
+    }
+    Children.prototype = new Parent()
+    Children.prototype.constructor = Children;
+    let person = new Children('tom')
+    let person2 = new Children('tom2')
+    person.arr.push(2)
+    //console.log(person2.arr)
+})();
+/*
+*一般属性放在实例上，方法放在原型上继承
+* 缺点：父类上的实例属性创建了两次，造成不必要的性能消耗
+* 缺点：一个子类更改了父类原型上的引用类型，导致所有子类都更改
+* */
+//4.改进的组合继承（寄生组合继承）
+(function () {
+    function Parent(name) {
+        this.name = name;
+    }
+    Parent.prototype.eat = function () {}
+    Parent.prototype.arr = [1]
+    function Children(name) {
+        Parent.call(this,name)
+    }
+    Children.prototype.__proto__ = Parent.prototype;
+    let person = new Children('tom')
+    let person2 = new Children('tom2')
+    person.arr.push(2)
+    //console.log(person2.arr)
+})();
+/*
+*改进了组合继承实例创建两次的问题
+* 缺点：一个子类更改了父类原型上的引用类型，导致所有子类都更改
+* */
+//5.原型继承实现多继承
+(function () {
+    function Parent() {
+    }
+    Parent.prototype.eat = function () {}
+    Parent.prototype.arr = [1]
+    function Parent2() {
+    }
+    Parent.prototype.color = 'red';
+    function Children(name) {
+        Parent.call(this,name)
+    }
+    function inherit(child,...args) {
+        let obj = {};
+        args.forEach(val=>{
+            obj = Object.assign(obj,val.prototype);
+        })
+        child.prototype.__proto__ = obj
+    }
+    //执行继承方法
+    inherit(Children,Parent)
+    let person = new Children('tom')
+    let person2 = new Children('tom2')
+    person.arr.push(2)
+    console.log(person2.arr)//[1,2]
+})();
+/*
+* 缺点：一个子类更改了父类原型上的引用类型，导致所有子类都更改
+* */
+//6.改造后的寄生继承
+(function () {
+    function Parent() {
+    }
+    Parent.prototype.eat = function () {}
+    Parent.prototype.arr = [1]
+    function Children(name) {
+        function child(){
+            Parent.call(this,name)
+        }
+        child.prototype.__proto__ = deepClone(Parent.prototype)
+        return new child()
+    }
+    let person = new Children('tom')
+    let person2 = new Children('tom2')
+    person.arr.push(2)
+    console.log(person2.arr) //[1]
+    console.log(person instanceof Parent)//false
+    console.log(person instanceof Children)//false
+    console.log(person.constructor === person2.constructor)//false
+    //深拷贝
+    function deepClone(obj,hash = new WeakMap()) {
+        //判断是否循环引用
+        if (hash.has(obj)) return hash.get(obj);
+        if(typeof obj === 'object' && obj !== null){
+            var o = Object.prototype.toString.call(obj) === '[object Array]'?[]:{};
+            hash.set(obj, o);
+            for(let key in obj){
+                if(typeof obj[key] === 'object'){
+                    o[key] = deepClone(obj[key],hash)
+                }else{
+                    o[key] = obj[key]
+                }
+            }
+            return o;
+        }else{
+            return obj;
+        }
+    }
+})();
+/*
+* 基本上解决了以上的问题
+* 缺点：子类的实例原型链上找不到父类构造函数，也找不到子类构造函数，且子类的构造函数不是同一个
+* */
+//7.extends关键字继承
+(function () {
+    class Parent {
+        arr = [1]
+        constructor(name){
+            this.name = name
+        }
+        eat(){}
+    }
+    class Children extends Parent {
+        constructor(name){
+            super(name)
+        }
+    }
+    let person = new Children('tom')
+    let person2 = new Children('tom2')
+    person.arr.push(2)
+    console.log(person2)
+})();
+/*
+* extends关键字继承的实现有些类似寄生组合方式，但是又有些不一样，extends把父类内部非函数属性放到放到子类实例上进行继承，函数放到原型上进行继承。
+*而且没有改变继承关系，算是一种完美方案
+* */
+//8.es5实现extends关键字继承（忽略class 的prototype上定义的属性）
+(function () {
+    function Parent(name) {
+        this.name = name;
+    }
+    Parent.prototype.eat = function () {}
+    Parent.prototype.arr = [1]
+    function Children(name) {
+        Parent.call(this,name);
+        for(let key in Parent.prototype){
+            if(typeof Parent.prototype[key] !== 'function'){
+                this[key] = deepClone(Parent.prototype[key]);
+            }
+        }
+    }
+    Children.prototype.__proto__ = Parent.prototype;
+    let person = new Children('tom')
+    let person2 = new Children('tom2')
+    person.arr.push(2)
+    console.log(person2.arr)//[1]
+    //深拷贝
+    function deepClone(obj,hash = new WeakMap()) {
+        //判断是否循环引用
+        if (hash.has(obj)) return hash.get(obj);
+        if(typeof obj === 'object' && obj !== null){
+            var o = Object.prototype.toString.call(obj) === '[object Array]'?[]:{};
+            hash.set(obj, o);
+            for(let key in obj){
+                if(typeof obj[key] === 'object'){
+                    o[key] = deepClone(obj[key],hash)
+                }else{
+                    o[key] = obj[key]
+                }
+            }
+            return o;
+        }else{
+            return obj;
+        }
+    }
+})();
+/*
+* 这种方式和extends还是有区别的，就是子类的实例上和原型上会有同名属性，不过父类实例只执行一次
+* */
